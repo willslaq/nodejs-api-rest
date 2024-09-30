2ecae5ce73cb5cd6994bdbf0503ca7bca8a46f2e
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-09-30 17:27:24 -0300

    made config of eslint and knex

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const test = await knex('sqlite_schema').select('*')
+  return { test }
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('server listening on port 3333')
+  })
