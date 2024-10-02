294501f41c4d4b387a7a3fe46b51f45948ce1c7d
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-10-01 22:59:07 -0300

    made route to insert new transaction with fastify plugin

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,15 @@
 import fastify from 'fastify'
-import { knex } from './database'
-
+import { env } from './env'
+import { transactionsRoutes } from './routes/transaction'
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions').select('*')
-
-  return transaction
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('server listening on port 3333')
